1ed6bc1bc661daeca1835b9ba59ca26a90e1d31a
blazej1679 <szczepanowski.blazej@gmail.com>
2019-05-20 22:00:38 +0200

    Create entry adding

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,14 +1,25 @@
 """Definiowanie wzorcow adresow URL dla aplikacji learning_logs."""
 
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     #strona glowna
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
+
     #wyswietlenie wszystkich tematow
-    url(r'^topics/$', views.topics, name='topics'),
-    url(r"^topics/(?P<topic_id>\d+)/$", views.topic, name='topic'),
-    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    #url(r'^topics/$', views.topics, name='topics'),
+    path('topics/', views.topics, name='topics'),
+
+    #url(r"^topics/(?P<topic_id>\d+)/$", views.topic, name='topic'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    path('new_topic/', views.new_topic, name='new_topic'),
+    #url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    #url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -6,13 +6,15 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from .models import Topic
+
 
 def index(request):
     """Strona glowna aplikacji learning_log."""
 
     return render(request, 'learning_logs/index.html')
+
 
 def topics(request):
     """Wyswietla wszystkie tematy."""
@@ -20,12 +22,14 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
 def topic(request, topic_id):
     """Wyswietla pojedynczy temat i wszytkie wpisy powiazane z nim."""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
 
 def new_topic(request):
     """Dodawanie nowego tematu."""
@@ -37,7 +41,26 @@ def new_topic(request):
         form = TopicForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('learning_logs:topics'))
+            return HttpResponseRedirect(reverse('topics'))
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Dodanie nowego wpisu przynależącego do tematu."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #Nie przekazano zadych danych, tworzenie pustego formularza.
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
